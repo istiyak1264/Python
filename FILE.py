2b2a63a292db7f6602d67b003b79c499a7e7646d
istiyak1264 <iamistiyakooo@gmail.com>
2025-01-31 10:41:42 +0600

    first commit

--- FILE.py
+++ FILE.py
@@ -1,7 +1,22 @@
 #!/bin/python3
 
-#opening a file in reading mode
+# Opening a file in reading mode
 f = open("sample.txt", "r")
+
+# Reading the full file
 data = f.read()
 print(data)
+
+# Moving the cursor back to the beginning before reading again
+f.seek(0)
+
+# Reading only 10 characters from the file
+data = f.read(10)
+print(data)
+
+# Reading one line at a time
+data = f.readline()
+print(data)
+
+# Closing the file
 f.close()
