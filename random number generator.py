479f1fb5027ec58facf7fea18638a16f1738a087
istiyak1264 <iamistiyakooo@gmail.com>
2025-02-02 13:17:51 +0600

    new commit

--- random number generator.py
+++ random number generator.py
@@ -1,9 +1,16 @@
 #!/bin/python3
 import random
 
+#generating random number using random function()
 target = random.randint(1, 100)
 while True:
-    UserChoice = int(input("Guess the Target: "))
+    UserChoice = input("Guess the Target or Quit(Q): ")
+    if(UserChoice == "Q"):
+        break
+    #converting UserChoice from string to integer
+    UserChoice = int(UserChoice)
+
+    #checkig the conditions
     if(target == UserChoice):
         print("Success: Correct Guess!!")
         break
@@ -14,4 +21,5 @@ while True:
     else:
         print("Your number was too big. Guess a little bit smaller number...")
 
+#when user successfully guess the number the game is over.
 print("-----------Game Over----------")
