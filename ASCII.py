3fcf94e3ab85efb052397e757647d1440528a018
istiyak1264 <iamistiyakooo@gmail.com>
2025-02-09 09:19:13 +0600

    cryptography

--- ASCII.py
+++ ASCII.py
@@ -1,9 +1,13 @@
-lists = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+#website: https://cryptohack.org/challenges/general/
+# problem name: ASCII
 
+lists = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+#declaring the flag as empty string
 flag = ""
 for i in lists:
+    #chr() function converts ascii value to character value
     flag += chr(i)
-
+#printing the flag
 print(flag)
 
 
---
+++ SSH keys.py
@@ -0,0 +1,13 @@
+# website: https://cryptohack.org/challenges/general/
+# problem name: SSH Keys
+
+from Crypto.PublicKey import RSA
+#opening the bruce_rsa.pub file in read mode
+with open("bruce_rsa.pub", "r") as f:
+    Key = f.read()
+    print(Key)
+
+    #importing the key
+    Key = RSA.import_key(Key)
+    #printing the modulus n as a decimal integer from Bruce's SSH public key.
+    print(Key.n)
