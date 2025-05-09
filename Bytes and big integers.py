5a62454d81a0dc4e1ce7148fed66e1c48d91f5c4
Istiyak Ahmed <iamistiyakooo@gmail.com>
2025-05-09 06:01:02 +0600

    update

--- Bytes and big integers.py
+++ Bytes and big integers.py
@@ -1,4 +1,4 @@
-from Crypto.Util.number import *
+from Cryptodome.Util.number import *
 
 encoded_flag = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
-print(long_to_bytes(encoded_flag))
+print(long_to_bytes(encoded_flag).decode())
